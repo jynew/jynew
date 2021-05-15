11db281628d7e564dd26ca4349ebb81d4de8b942
linkinmama <linkinmama@gmail.com>
2021-05-15 17:14:52 +0800

    交互界面中“交互”按钮和“使用物品”按钮可以同时出现了

--- jyx2/Assets/Scripts/Jyx2UIScripts/InteractUIPanel.cs
+++ jyx2/Assets/Scripts/Jyx2UIScripts/InteractUIPanel.cs
@@ -1,34 +1,42 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 
-public partial class InteractUIPanel:Jyx2_UIBase
+public partial class InteractUIPanel : Jyx2_UIBase
 {
     public override UILayer Layer => UILayer.NormalUI;
 
-    Action m_callback;
+    Action m_callback1;
+    Action m_callback2;
+
     protected override void OnCreate()
     {
         InitTrans();
 
-        BindListener(MainBg_Button, OnBtnClick);
+        BindListener(MainBg_Button1, () => OnBtnClick(0));
+        BindListener(MainBg_Button2, () => OnBtnClick(1));
     }
 
     protected override void OnShowPanel(params object[] allParams)
     {
         base.OnShowPanel(allParams);
 
-        string mainText = allParams[0] as string;
-        m_callback = allParams[1] as Action;
+        if (allParams == null) return;
 
-        MainText_Text.text = mainText;
+        int buttonCount = allParams.Length / 2;
+        MainBg_Button2.gameObject.SetActive(buttonCount == 2);
+
+        MainText_Text1.text = allParams[0] as string;
+        m_callback1 = allParams[1] as Action;
+
+        if (MainBg_Button2.gameObject.activeInHierarchy)
+        {
+            MainText_Text2.text = allParams[2] as string;
+            m_callback2 = allParams[3] as Action;
+        }
     }
 
-    void OnBtnClick() 
+    void OnBtnClick(int buttonIndex)
     {
-        Action temp = m_callback;
+        Action temp = buttonIndex == 0 ? m_callback1 : m_callback2;
         Jyx2_UIManager.Instance.HideUI("InteractUIPanel");
         temp?.Invoke();
     }
@@ -36,6 +44,7 @@ public partial class InteractUIPanel:Jyx2_UIBase
     protected override void OnHidePanel()
     {
         base.OnHidePanel();
-        m_callback = null;
+        m_callback1 = null;
+        m_callback2 = null;
     }
 }
--- jyx2/Assets/Scripts/Jyx2UIScripts/UIData/InteractUIPanel_UIData.cs
+++ jyx2/Assets/Scripts/Jyx2UIScripts/UIData/InteractUIPanel_UIData.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public partial class InteractUIPanel
 {
-	private RectTransform InteractiveButton_RectTransform;
-	private Button MainBg_Button;
-	private Text MainText_Text;
+	private RectTransform InteractiveButton_RectTransform1;
+	private Button MainBg_Button1;
+	private Text MainText_Text1;
+
+	private RectTransform InteractiveButton_RectTransform2;
+	private Button MainBg_Button2;
+	private Text MainText_Text2;
 
 	public void InitTrans()
 	{
-		InteractiveButton_RectTransform = transform.Find("BtnRoot/InteractiveButton").GetComponent<RectTransform>();
-		MainBg_Button = transform.Find("BtnRoot/InteractiveButton/MainBg").GetComponent<Button>();
-		MainText_Text = transform.Find("BtnRoot/InteractiveButton/MainBg/MainText").GetComponent<Text>();
+		InteractiveButton_RectTransform1 = transform.Find("BtnRoot/InteractiveButton1").GetComponent<RectTransform>();
+		MainBg_Button1 = transform.Find("BtnRoot/InteractiveButton1/MainBg").GetComponent<Button>();
+		MainText_Text1 = transform.Find("BtnRoot/InteractiveButton1/MainBg/MainText").GetComponent<Text>();
 
+		InteractiveButton_RectTransform2 = transform.Find("BtnRoot/InteractiveButton2").GetComponent<RectTransform>();
+		MainBg_Button2 = transform.Find("BtnRoot/InteractiveButton2/MainBg").GetComponent<Button>();
+		MainText_Text2 = transform.Find("BtnRoot/InteractiveButton2/MainBg/MainText").GetComponent<Text>();
 	}
 }
