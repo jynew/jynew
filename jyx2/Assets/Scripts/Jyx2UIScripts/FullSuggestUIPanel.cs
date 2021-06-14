696a9796cbb53955f022315258320ab78b32956a
eaphone Cai-at gamingStdio <cyfdel@hotmail.com>
2021-06-14 12:57:47 +0800

    优化战斗结算画面分页显示

--- jyx2/Assets/Scripts/Jyx2UIScripts/FullSuggestUIPanel.cs
+++ jyx2/Assets/Scripts/Jyx2UIScripts/FullSuggestUIPanel.cs
@@ -10,7 +10,8 @@ public partial class FullSuggestUIPanel:Jyx2_UIBase
 
     Action m_callback;
     string m_content = "";
-    string m_title = "";
+    int curIndex=0;
+	string m_title = "";
     float m_canClickTime = 0;
     protected override void OnCreate()
     {
@@ -22,23 +23,27 @@ public partial class FullSuggestUIPanel:Jyx2_UIBase
     {
         if (Time.unscaledTime < m_canClickTime)
             return;
-        Action cb = m_callback;
-        Jyx2_UIManager.Instance.HideUI("FullSuggestUIPanel");
-        cb?.Invoke();
+		if(curIndex<m_content.Length){
+			Content_Text.text = GetSubContent();
+		}else{
+			Action cb = m_callback;
+			Jyx2_UIManager.Instance.HideUI("FullSuggestUIPanel");
+			cb?.Invoke();
+		}
     }
 
     protected override void OnShowPanel(params object[] allParams)
     {
         base.OnShowPanel(allParams);
 
-        m_content = allParams[0] as string;
+		m_content = (allParams[0] as string).Trim();
         if(allParams.Length > 1)
             m_title = allParams[1] as string;
         if(allParams.Length > 2)
             m_callback = allParams[2] as Action;
 
         Title_Text.text = m_title;
-        Content_Text.text = m_content;
+		Content_Text.text = GetSubContent();
         m_canClickTime = Time.unscaledTime + 0.5f;//0.5s后才能点击
         GameUtil.GamePause(true);
     }
@@ -48,7 +53,22 @@ public partial class FullSuggestUIPanel:Jyx2_UIBase
         base.OnHidePanel();
         m_title = "";
         m_content = "";
+		curIndex=0;
         m_callback = null;
         GameUtil.GamePause(false);
     }
+	
+	private string GetSubContent(){
+		int endIndex=curIndex-1;
+		for(int i=0;i<GameConst.MAX_BATTLE_RESULT_LINE_NUM;i++){
+			endIndex=m_content.IndexOfAny(new char[]{'\r','\n'},endIndex+1);
+			if(endIndex==-1){
+				endIndex=m_content.Length;
+				break;
+			}
+		}
+		var result=m_content.Substring(curIndex,endIndex-curIndex);
+		curIndex=endIndex+1;
+		return result;
+	}
 }
