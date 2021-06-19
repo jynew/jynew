deb03704bf36e711116d036188b641c6c00647bc
eaphone Cai-at gamingStdio <cyfdel@hotmail.com>
2021-06-19 23:43:17 +0800

    优化沙漠废墟和高昌迷宫场景

--- jyx2/data/lua/jygame/ka79.lua
+++ jyx2/data/lua/jygame/ka79.lua
@@ -1,6 +1,5 @@
 SetScenceMap(-2, 1, 29, 22, 0);--by fanyu|明教地道出现，贴图变化。场景12-编号12
-jyx2_ReplaceSceneObject("", "Dynamic/cushion-ori", ""); 
-jyx2_ReplaceSceneObject("", "Dynamic/cushion-moved", "1"); 
+jyx2_MovePlayer("cushion-moved","Level/Dynamic","Level/Dynamic/cushion-ori")
 SetScenceMap(-2, 1, 29, 21, 2898);--by fanyu|明教地道出现，贴图变化。场景12-编号12
 ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -1, -1, -1, -2, -2, -2);
 jyx2_ReplaceSceneObject("", "FX/didaoEntrance", "1"); 
