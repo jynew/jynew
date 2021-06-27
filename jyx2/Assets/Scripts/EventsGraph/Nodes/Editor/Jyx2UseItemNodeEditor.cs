906f89ceb48b5a1d3fa7780d5cc50b9a8197e64a
CGGG <chenggong8@gmail.com>
2021-06-27 12:38:57 +0800

    代码重构：抽取EditorNode相关组件显示的工具类 #218

--- jyx2/Assets/Scripts/EventsGraph/Nodes/Editor/Jyx2UseItemNodeEditor.cs
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Editor/Jyx2UseItemNodeEditor.cs
@@ -11,23 +11,15 @@ using XNodeEditor;
 [CustomNodeEditor(typeof(Jyx2UseItemNode))]
 public class Jyx2UseItemNodeEditor : NodeEditor
 {
-    private Jyx2UseItemNode myNode;
-    private int _selectedIndex = 0;
-
+    private Jyx2NodeEditorHelperItem _itemDrawer;
     public override void OnCreate()
     {
         base.OnCreate();
-        if (myNode == null) myNode = target as Jyx2UseItemNode;
-        serializedObject.Update();
-        _selectedIndex = serializedObject.FindProperty("itemId").intValue;
-        
-        EditorStyles.textField.wordWrap = true; // 自动换行
+        _itemDrawer = new Jyx2NodeEditorHelperItem(this);
     }
 
-    public override void OnBodyGUI() {
-        
-        if (myNode == null) myNode = target as Jyx2UseItemNode;
-        
+    public override void OnBodyGUI() 
+    {
         // Update serialized object's representation
         serializedObject.Update();
         
@@ -35,30 +27,9 @@ public class Jyx2UseItemNodeEditor : NodeEditor
         NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("yes"));
         NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("no"));
         
-        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("itemId"));
-
-        _selectedIndex = serializedObject.FindProperty("itemId").intValue;
+        _itemDrawer.DrawAll();
         
-        //道具图标
-        int sel = EditorGUILayout.Popup("道具", _selectedIndex, Jyx2EventsGraphStatic.s_itemList);
-        if (sel != _selectedIndex)
-        {
-            serializedObject.FindProperty("itemId").intValue = sel;
-            _selectedIndex = sel;
-        }
-        
-        //道具图标
-        var itemContent = new GUIContent(GetItemTexture());
-        EditorGUILayout.LabelField(itemContent, GUILayout.Height(50));
-
         // Apply property modifications
         serializedObject.ApplyModifiedProperties();
-    }
-    
-    public Texture2D GetItemTexture()
-    {
-        int id = serializedObject.FindProperty("itemId").intValue;
-
-        return AssetDatabase.LoadAssetAtPath<Texture2D>($"Assets/BuildSource/Jyx2Items/{id}.png");
     }
 }
