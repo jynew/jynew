a1956d926b8e3c0acb99c7a97a0bdb72fd786e7e
CGGG <chenggong8@gmail.com>
2021-07-10 11:41:27 +0800

    导入新制作的动作，并且进行配置。

---
+++ jyx2/Assets/3D/Animation/README.md
@@ -0,0 +1,3 @@
+
+注意：
+本目录下所有的资源最终都是临时的、需要删除的，Animations都应该关联到BuildSource/Animations目录下，而非直接在此引用。
---
+++ jyx2/Assets/Editor/AnimationTools/AnimationTools.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimationTools
+{
+    [MenuItem("Tools/动画工具/一键配置所有动作为JYX2标准")]
+    static public void ForceCheckAllJyx2Animations()
+    {
+        
+        foreach (var file in Directory.GetFiles("Assets/BuildSource/Animations"))
+        {
+            if (!file.EndsWith(".anim")) continue;
+
+            
+            var clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(file);
+
+            if (clip != null)
+            {
+                AnimationClipSettings acs = AnimationUtility.GetAnimationClipSettings(clip);
+                acs.keepOriginalOrientation = true;
+                acs.keepOriginalPositionY = true;
+                acs.keepOriginalPositionXZ = true;
+
+                acs.loopBlendOrientation = true;
+                acs.loopBlendPositionY = true;
+                acs.loopBlendPositionXZ = true;
+                
+                if (clip.name.Contains("站立") || clip.name.Contains("待机") || clip.name.Contains("移动"))
+                {
+                    acs.loopTime = true;
+                }
+                else
+                {
+                    acs.loopTime = false;
+                }
+                
+                AnimationUtility.SetAnimationClipSettings(clip, acs);
+            }
+            AssetDatabase.SaveAssets();
+            Debug.Log(clip);
+        }
+    }
+}
