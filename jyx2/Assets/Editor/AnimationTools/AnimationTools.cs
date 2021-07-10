b5fab9c3567bd63c3e2ddcdc5e923fdae12dfb19
CGGG <chenggong8@gmail.com>
2021-07-10 12:13:35 +0800

    Update AnimationTools.cs

--- jyx2/Assets/Editor/AnimationTools/AnimationTools.cs
+++ jyx2/Assets/Editor/AnimationTools/AnimationTools.cs
@@ -27,20 +27,33 @@ public class AnimationTools
                 acs.loopBlendOrientation = true;
                 acs.loopBlendPositionY = true;
                 acs.loopBlendPositionXZ = true;
-                
-                if (clip.name.Contains("站立") || clip.name.Contains("待机") || clip.name.Contains("移动") || clip.name.Contains("跑图"))
-                {
-                    acs.loopTime = true;
-                }
-                else
-                {
-                    acs.loopTime = false;
-                }
+
+                acs.loopTime = IsClipLoopAnimation(clip);
                 
                 AnimationUtility.SetAnimationClipSettings(clip, acs);
             }
             AssetDatabase.SaveAssets();
             Debug.Log(clip);
+        }
+    }
+
+
+    /// <summary>
+    /// 一个动作是否是循环播放的动作
+    ///
+    /// 目前先是使用这种苟且写死在代码里的方式，有需要的话可以调整
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <returns></returns>
+    static private bool IsClipLoopAnimation(AnimationClip clip)
+    {
+        if (clip.name.Contains("站立") || clip.name.Contains("待机") || clip.name.Contains("移动") || clip.name.Contains("跑图"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
