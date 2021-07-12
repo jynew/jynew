04d1f7bdc349eb70d40f0cfc9e49a6fdf2d1dc4a
zyjdxtc <zyjdxtc@hotmail.com>
2021-07-12 13:57:02 -0400

    add Jyx2ZeroAllMPNode

---
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2ZeroAllMPNode.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HanSquirrel.ResourceManager;
+using HSFrameWork.ConfigTable;
+using Jyx2;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("游戏数据/清空全队内力")]
+[NodeWidth(150)]
+public class Jyx2ZeroAllMPNode : Jyx2SimpleNode
+{
+	private void Reset() {
+		name = "清空全队内力";
+	}
+
+
+	protected override void DoExecute()
+	{
+		Jyx2LuaBridge.ZeroAllMP();
+	}
+}
