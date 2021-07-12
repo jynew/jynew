7cb54e6a8dcf75f76c119d602cd62fca50cc63c4
eaphone Cai-at gamingStdio <cyfdel@hotmail.com>
2021-07-12 13:38:51 +0800

    修改地道入口处理逻辑

--- jyx2/data/lua/jygame/ka654.lua
+++ jyx2/data/lua/jygame/ka654.lua
@@ -1,9 +1,7 @@
 ModifyEvent(-2, -2, 0, 0, -1, -1, -1, -1, -1, -1, -2, -2, -2);
 SetScenceMap(-2, 1, 18, 21, 4576);--by fanyu|贴图改变。场景15
--- eaphone at 2021/6/3
-jyx2_ReplaceSceneObject("","Dynamic/Sark_6_ori","");
-jyx2_ReplaceSceneObject("","Dynamic/Sark_6_moved","1");
 SetScenceMap(-2, 1, 19, 21, 0);--by fanyu|贴图改变。场景15
-jyx2_ReplaceSceneObject("", "FX/migongEntrance", "1"); 
-jyx2_ReplaceSceneObject("", "Triggers/Leave2", "1"); 
+jyx2_FixMapObject("高昌迷宫入口出现",1);
+jyx2_ReplaceSceneObject("", "Dynamic/migongEntrance", "1"); 
+jyx2_ReplaceSceneObject("", "Dynamic/Leave2", "1"); 
 do return end;
--- jyx2/data/lua/jygame/ka79.lua
+++ jyx2/data/lua/jygame/ka79.lua
@@ -1,8 +1,7 @@
 SetScenceMap(-2, 1, 29, 22, 0);--by fanyu|明教地道出现，贴图变化。场景12-编号12
 SetScenceMap(-2, 1, 29, 21, 2898);--by fanyu|明教地道出现，贴图变化。场景12-编号12
-jyx2_ReplaceSceneObject("", "Dynamic/cushion-ori", "0"); 
-jyx2_ReplaceSceneObject("", "Dynamic/cushion-moved", "1"); 
+jyx2_FixMapObject("明教地道入口出现",1);
+jyx2_ReplaceSceneObject("", "Dynamic/didaoEntrance", "1"); 
+jyx2_ReplaceSceneObject("", "Dynamic/Leave2", "1"); 
 ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -1, -1, -1, -2, -2, -2);
-jyx2_ReplaceSceneObject("", "FX/didaoEntrance", "1"); 
-jyx2_ReplaceSceneObject("", "Triggers/Leave2", "1"); 
 do return end;
