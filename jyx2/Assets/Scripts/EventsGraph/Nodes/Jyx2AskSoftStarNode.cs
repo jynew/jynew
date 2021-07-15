961c175181c8fea5b8fd1a1fbcc46d8a86208d12
zyjdxtc <zyjdxtc@hotmail.com>
2021-07-15 10:39:54 -0400

    fix Jyx2AskSoftStarNode

--- jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2AskSoftStarNode.cs
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2AskSoftStarNode.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using HanSquirrel.ResourceManager;
+using HSFrameWork.ConfigTable;
+using Jyx2;
 using UnityEngine;
+using XNode;
 
-public class Jyx2AskSoftStarNode : MonoBehaviour
+[CreateNodeMenu("场景/和软体娃娃对话")]
+[NodeWidth(150)]
+public class Jyx2AskSoftStarNode : Jyx2SimpleNode
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private void Reset() {
+		name = "和软体娃娃对话";
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	protected override void DoExecute()
+	{
+		Jyx2LuaBridge.AskSoftStar();
+	}
 }
