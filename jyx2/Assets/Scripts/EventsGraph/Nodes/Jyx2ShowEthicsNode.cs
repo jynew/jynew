3d23a44c3cb536dafc8e41eab86cde45799dd0ca
zyjdxtc <zyjdxtc@hotmail.com>
2021-07-15 10:07:15 -0400

    Jyx2ShowEthicsNode Jyx2ShowReputeNode

---
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2ShowEthicsNode.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HanSquirrel.ResourceManager;
+using HSFrameWork.ConfigTable;
+using Jyx2;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("游戏数据/显示道德值")]
+[NodeWidth(150)]
+public class Jyx2ShowEthicsNode : Jyx2SimpleNode
+{
+	private void Reset() {
+		name = "显示道德值";
+	}
+
+
+	protected override void DoExecute()
+	{
+		Jyx2LuaBridge.ShowEthics();
+	}
+}
