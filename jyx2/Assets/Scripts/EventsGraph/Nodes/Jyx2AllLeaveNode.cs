c4b6537cb3e4e1bf0d1dbb7e055ed44a264d930a
zyjdxtc <zyjdxtc@hotmail.com>
2021-07-15 11:01:40 -0400

    FightForTop, AllLeave Nodes

---
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2AllLeaveNode.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HanSquirrel.ResourceManager;
+using HSFrameWork.ConfigTable;
+using Jyx2;
+using UnityEngine;
+using XNode;
+
+[CreateNodeMenu("游戏数据/全体队友离队")]
+[NodeWidth(180)]
+public class Jyx2AllLeaveNode : Jyx2SimpleNode
+{
+    private void Reset() {
+        name = "全体队友离队";
+    }
+    
+    protected override void DoExecute()
+	{   
+		Jyx2LuaBridge.AllLeave(roleId);
+	}
+}
