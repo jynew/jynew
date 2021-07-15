71006298188f1aefc66d34f50fd65a692fe561b1
zyjdxtc <zyjdxtc@hotmail.com>
2021-07-15 10:34:43 -0400

    Jyx2AskSoftStarNode Jyx2OpenAllSceneNode

--- jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2OpenAllSceneNode.cs
+++ jyx2/Assets/Scripts/EventsGraph/Nodes/Jyx2OpenAllSceneNode.cs
@@ -7,18 +7,16 @@ using Jyx2;
 using UnityEngine;
 using XNode;
 
-[CreateNodeMenu("场景/标记场景可以进入")]
-[NodeWidth(200)]
-public class Jyx2OpenSceneNode : Jyx2SimpleNode
+[CreateNodeMenu("场景/打开所有场景")]
+[NodeWidth(150)]
+public class Jyx2OpenAllSceneNode : Jyx2SimpleNode
 {
     private void Reset() {
-        name = "标记场景可以进入";
+        name = "打开所有场景";
     }
-    [Header("场景id")]
-    public int sceneId;
     
     protected override void DoExecute()
 	{   
-		Jyx2LuaBridge.OpenScene(sceneId);
+		Jyx2LuaBridge.OpenAllScene();
 	}
 }
