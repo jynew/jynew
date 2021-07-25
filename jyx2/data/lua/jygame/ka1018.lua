f750c4ff31d1b69a268bb836b98a963359100ab2
eaphone Cai-at gamingStdio <cyfdel@hotmail.com>
2021-07-25 22:44:55 +0800

    补充提交新增文件

---
+++ jyx2/data/lua/jygame/ka1018.lua
@@ -0,0 +1,6 @@
+if jyx2_CheckBookAndRepute() == true then goto label0 end;
+    do return end;
+::label0::
+    ModifyEvent(-2,11,0,0,932,-1,-1,7968,-1,-1,-1,-1,-1);
+	jyx2_ReplaceSceneObject("","Dynamic/请帖","1");
+do return end;
