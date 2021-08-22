52e6d4ef88128800e146c56f8666d1c1583a0d64
CGGG <chenggong8@gmail.com>
2021-08-22 11:11:11 +0800

    修复缺少ios宏编译报错的问题

--- jyx2/Assets/NativeFileSO/Editor/NativeFileSOBuild.cs
+++ jyx2/Assets/NativeFileSO/Editor/NativeFileSOBuild.cs
@@ -1,4 +1,5 @@
-﻿// 	Copyright (c) 2019 Keiwan Donyagard
+﻿#if UNITY_IOS
+// 	Copyright (c) 2019 Keiwan Donyagard
 // 
 //  This Source Code Form is subject to the terms of the Mozilla Public
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -216,3 +217,4 @@ public class NativeFileSOBuild {
 	}
 }
 
+#endif
