463d970822c1403805831ac57d84573bb4ae4733
EasternDay <849919718@qq.com>
2021-12-23 00:39:41 +0800

    1️⃣稍微重构了一下MapRole及其继承的脚本AnimationBattleRole，重写了眩晕并测试。 2️⃣SkillEditorUIPanel逻辑精简。 3️⃣GlobalAssetConfig和SkillDisplayAsset完善逻辑并添加Idle动作和Stun动作。

--- jyx2/Assets/Scripts/Jyx2AnimationBattleRole.cs
+++ jyx2/Assets/Scripts/Jyx2AnimationBattleRole.cs
@@ -24,13 +24,11 @@ namespace Jyx2
         private HybridAnimancerComponent _animancer;
         public HybridAnimancerComponent GetAnimancer()
         {
-            if (_animancer == null)
-            {
-                var animator = GetAnimator();
-                _animancer = GameUtil.GetOrAddComponent<HybridAnimancerComponent>(animator.transform);
-                _animancer.Animator = animator;
-                _animancer.Controller = animator.runtimeAnimatorController;
-            }
+            //TODO:判断是否销毁了_animacer，替换模型_animacer不会立即消失……,所以直接每次都get一次
+            var animator = GetAnimator();
+            _animancer = GameUtil.GetOrAddComponent<HybridAnimancerComponent>(animator.transform);
+            _animancer.Animator = animator;
+            _animancer.Controller = animator.runtimeAnimatorController;
             return _animancer;
         }
         
