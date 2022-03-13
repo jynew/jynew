73f191ee12a7d451d5d2df8005ac80cc460dee07
qige2016 <286882998@qq.com>
2022-03-13 09:49:38 +0800

    修复：打完74的丘处机，无法触发75的再战全真的问题

--- jyx2/Assets/BuildSource/Lua/jygame/ka458.lua
+++ jyx2/Assets/BuildSource/Lua/jygame/ka458.lua
@@ -1,7 +1,7 @@
 if JudgeEthics(0, 50, 100) == false then goto label0 end;
     Talk(68, "最近江湖上对你的风评还挺不错的，希望你继续保持下去.", "talkname68", 0);
-::label0::
     do return end;
+::label0::
     Talk(68, "你这作恶多端的小子，老道饶不了你．", "talkname68", 0);
     if TryBattle(75) == true then goto label1 end;
         LightScence();
