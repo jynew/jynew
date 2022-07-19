c8241fc891867044b9b46024e2500179689e90fb
qige2016 <286882998@qq.com>
2022-07-19 13:35:24 +0800

    新增：支持触发器和跳转场景映射关系可配置

--- jyx2/Assets/Scripts/Jyx2UIScripts/MainUIPanel.cs
+++ jyx2/Assets/Scripts/Jyx2UIScripts/MainUIPanel.cs
@@ -278,7 +278,7 @@ public partial class MainUIPanel : Jyx2_UIBase, IUIAnimator
 		//执行离开事件
 		foreach (var zone in FindObjectsOfType<MapTeleportor>())
 		{
-			if (LevelMaster.GetCurrentGameMap().TransportToMap == GameConst.WORLD_MAP_ID)
+			if (LevelMaster.GetCurrentGameMap().Tags.Contains("WORLDMAP"))
 			{
 				zone.DoTransport();
 				break;
