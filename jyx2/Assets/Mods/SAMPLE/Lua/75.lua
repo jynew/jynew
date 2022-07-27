f9527d52c863107ddd24fdfe9acc8f90b27aa279
qige2016 <286882998@qq.com>
2022-07-28 00:15:38 +0800

    新增罂粟谷

---
+++ jyx2/Assets/Mods/SAMPLE/Lua/75.lua
@@ -0,0 +1,16 @@
+Talk(70, "有幸结识如此潇洒威武的少侠，如果需要帮助，随时可以叫我。");
+if AskJoin () == true then goto label0 end;
+    Talk(0, "罢了。");
+    ModifyEvent(-2, -2, -2, -2, 75, -1, -1, -2, -2, -2, -2, -2, -2);
+    do return end;
+::label0::
+    if TeamIsFull() == false then goto label1 end;
+        Talk(70, "你的队伍已满，我无法加入。");
+        do return end;
+::label1::
+    Talk(0, "我还有未完成的事情，那你随我一起吧。");
+    jyx2_ReplaceSceneObject("","NPC/王语嫣","");
+    LightScence();
+    Join(70);
+    ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -2, -2, -2, -2, -2, -2);
+do return end;
