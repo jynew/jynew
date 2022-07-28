87dbcca48008cbb06e01ace1472babf9b9116b81
qige2016 <286882998@qq.com>
2022-07-28 13:31:38 +0800

    优化罂粟谷

---
+++ jyx2/Assets/Mods/SAMPLE/Lua/79.lua
@@ -0,0 +1,10 @@
+if InTeam(80) == true then goto label0 end;
+    Talk(170, "请离开吧。。。");
+    do return end;
+::label0::
+    Talk(80, "娘！原来你是我娘！");
+    Talk(170, "哎，看来你都知道了。");
+    Talk(80, "我长这么大都没有和娘在一起生活过，六一兄，对不住了，我先不陪你了，我想在这里陪陪我娘。");
+    Talk(0, "好的！兄弟保重！");
+    jyx2_ReplaceSceneObject("", "NPC/石破天", "1");
+do return end;
