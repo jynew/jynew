73721a799ed2cb9bb62bede57ee4ad6229e3a5d8
qige2016 <286882998@qq.com>
2022-08-24 22:58:36 +0800

    修改项

---
+++ jyx2/Assets/Mods/SAMPLE/Lua/113.lua
@@ -0,0 +1,3 @@
+AddItem(11, 1);
+ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -2, -2, -2, -2, -2, -2);
+do return end;
---
+++ jyx2/Assets/Mods/SAMPLE/Lua/811.lua
@@ -0,0 +1,3 @@
+AddItem(81, 5);
+ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -2, -2, -2, -2, -2, -2);
+do return end;
