4a9d54432ca24c23987cd70027a843b544cd83b1
qige2016 <286882998@qq.com>
2022-10-06 21:00:35 +0800

    优化炼成

--- jyx2/Assets/Mods/SAMPLE/Lua/123.lua
+++ jyx2/Assets/Mods/SAMPLE/Lua/123.lua
@@ -4,10 +4,4 @@ AddItem(121, 10);
 AddItem(174, 100);
 ModifyEvent(-2, -2, -2, -2, -1, -1, -1, -2, -2, -2, -2, -2, -2);
 ModifyEvent(-2, -2, -2, -2, 124, -1, -1, -2, -2, -2, -2, -2, -2);
-if InTeam(123) == true then goto label0 end;
-    do return end;
-::label0::
-    jyx2_ReplaceSceneObject("", "NPC/朱云天", "1");
-    Talk(1120, "夫君，出门在外，难免有跌打损伤。这个《渡城药鉴》你带上，说不定有用。");
-    AddItem(124, 1);
 do return end;
