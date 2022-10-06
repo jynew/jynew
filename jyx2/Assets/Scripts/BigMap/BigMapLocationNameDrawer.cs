9bad3af5e945db32069e3275b4b44a7b710ffe31
CGGG <chenggong8@gmail.com>
2022-10-07 02:14:38 +0800

    增加跳过地点名字显示的语法

--- jyx2/Assets/Scripts/BigMap/BigMapLocationNameDrawer.cs
+++ jyx2/Assets/Scripts/BigMap/BigMapLocationNameDrawer.cs
@@ -16,12 +16,8 @@ public class BigMapLocationNameDrawer : MonoBehaviour
         var allLocs = FindObjectsOfType<MapTeleportor>();
         foreach (var loc in allLocs)
         {
-            //禁止地点名称
-            if (Jyx2LuaBridge.jyx2_GetFlagInt($"BanLocationName.{loc.name}") == 1)
-            {
-                continue;
-            }
-            
+            if (JudgeIfIgnoreLocationNameDisplay(loc)) continue;
+
             var nameObj = Instantiate(m_NameTextPrefab);
             nameObj.transform.position = loc.transform.position + Vector3.up * 6;
             nameObj.transform.localScale = Vector3.one * 3;
@@ -42,5 +38,39 @@ public class BigMapLocationNameDrawer : MonoBehaviour
                 nameObj.GetComponent<TextMesh>().text = loc.name;    
             }
         }
+    }
+
+    /// <summary>
+    /// 判断是否要跳过名字显示
+    ///
+    /// 目前支持的方法是在lua中设置flag
+    ///
+    /// - 禁止显示所有的地点，只显示田伯光居：
+    /// SetFlag("BanLocationName.All",1)
+    /// SetFlag("ShowLocationName.田伯光居",1)
+    ///
+    /// - 显示所有地点，只禁用田伯光居：
+    /// SetFlag("BanLocationName.All",0)
+    /// SetFlag("BanLocationName.田伯光居",1)
+    /// 
+    /// </summary>
+    /// <param name="loc"></param>
+    /// <returns></returns>
+    private static bool JudgeIfIgnoreLocationNameDisplay(MapTeleportor loc)
+    {
+        //全部禁止显示，只过滤设置了ShowLocationName的
+        if (Jyx2LuaBridge.jyx2_GetFlagInt($"BanLocationName.All") == 1 &&
+            Jyx2LuaBridge.jyx2_GetFlagInt($"ShowLocationName.{loc.name}") == 0)
+        {
+            return true;
+        }
+
+        //禁止地点名称
+        if (Jyx2LuaBridge.jyx2_GetFlagInt($"BanLocationName.{loc.name}") == 1)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
