6d1fc6d5b185e7647c15ed78d39d4f18a5f15321
CGGG <chenggong8@gmail.com>
2022-10-10 00:19:09 +0800

    Update Generator.cs

--- jyx2/Assets/XLua/Src/Editor/Generator.cs
+++ jyx2/Assets/XLua/Src/Editor/Generator.cs
@@ -1461,7 +1461,13 @@ namespace CSObjectWrapEditor
 
             CSharpCallLua = new List<Type>()
             {
-                typeof(Action<int>)
+                typeof(Action<int>),
+                typeof(Action<string>),
+                typeof(Action<float>),
+                typeof(Action<bool>),
+                typeof(Action<long>),
+                typeof(Action<uint>),
+                typeof(Action<ulong>),
             };
 
             GCOptimizeList = new List<Type>();
