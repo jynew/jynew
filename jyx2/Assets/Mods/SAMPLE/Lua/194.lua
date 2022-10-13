c242b77625180647fad26710711ddb95f030a27c
qige2016 <286882998@qq.com>
2022-10-13 20:10:00 +0800

    暂存无际坊

---
+++ jyx2/Assets/Mods/SAMPLE/Lua/194.lua
@@ -0,0 +1,7 @@
+if InTeam(11) == true then goto label0 end;
+    Talk(1191, "我爹马上要举行比武招亲了，也不知我会嫁给什么样的人，真希望是一位的兼资文武、品貌不凡的公子。");
+    do return end;
+::label0::
+    Talk(1191, "那朱公子，要加油哦。");
+    Talk(11, "好！好！");
+do return end;
