22b08fe5d993c8f9c7ca08867928d5c8a09e7789
0kk470 <2549366978@qq.com>
2022-12-06 14:07:32 +0800

    基础包在启动时预加载下，临时这么处理，每次启动mod时baseasset不应该清理

--- jyx2/Assets/Scripts/Jyx2UIScripts/ModPanelLauncher.cs
+++ jyx2/Assets/Scripts/Jyx2UIScripts/ModPanelLauncher.cs
@@ -1,4 +1,5 @@
-﻿using MOD.UI;
+﻿using Jyx2.ResourceManagement;
+using MOD.UI;
 using System;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ namespace Jyx2
     { 
         private async void Start()
         {
+            await ResLoader.Init(); //UI资源在基础包里要初始化下
             await Jyx2_UIManager.Instance.ShowUIAsync<ModPanelNew>();
         }
     }
