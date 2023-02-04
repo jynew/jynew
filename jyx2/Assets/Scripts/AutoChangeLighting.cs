2a844e0bd420b0157cc843e8a84ab3abc1165932
citydream163 <citydream@163.com>
2023-02-04 10:54:25 +0800

    Update AutoChangeLighting.cs

--- jyx2/Assets/Scripts/AutoChangeLighting.cs
+++ jyx2/Assets/Scripts/AutoChangeLighting.cs
@@ -2,27 +2,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class AutoChangeLighting : MonoBehaviour
+namespace Jyx2
 {
-    public GameObject MainLight;
-    public float _RealGameTimeCount = 0;
-    public bool isCount = false;
-    public float TimeChangerate = 5f;//默认游戏时间5分钟等于游戏内1天
-    public float time2angle = 1.2f;//日照5分钟循环一次 --360/(TimeChangerate * 60)
-    public int _GameDayCount = int.Parse(Jyx2.Jyx2LuaBridge.jyx2_GetFlag("GameDayCount"));//读取日期数据
-    void Update()
+    public class AutoChangeLighting : MonoBehaviour
     {
-        if (isCount)
+        public GameObject MainLight;
+        public float _RealGameTimeCount = 0;
+        public bool isCount = false;
+        public float TimeChangerate = 300f;//默认游戏时间5分钟等于游戏内1天
+        private float timetoangle;
+        public string GamedaycountString="Gamedaycount";//自定义保存日期数据的关键字
+        public int _GameDayCount;//读取日期数据
+        //public float Rotatex;//旋转角度的值x
+        private void Start()
         {
-        _RealGameTimeCount += Time.deltaTime;
-            if (_RealGameTimeCount >= TimeChangerate*60)//5分钟1天
+            if (isCount) 
             {
-                _GameDayCount += 1; //日计数器累加
-                Jyx2.Jyx2LuaBridge.jyx2_SetFlag("GameDayCount", _GameDayCount.ToString());//存储日期数据
-                _RealGameTimeCount = 0;
+                timetoangle = 360 / TimeChangerate;//时间和角度换算关系
+                if (GamedaycountString != null)
+                {
+                    _GameDayCount = int.Parse(Jyx2.Jyx2LuaBridge.jyx2_GetFlag(GamedaycountString));//读取日期数据
+                }
             }
         }
-        MainLight.transform.Rotate(Vector3.right * Time.deltaTime * time2angle);
+        private void Update()
+        {
+            if (isCount)
+            {
+                _RealGameTimeCount += Time.deltaTime;
+                if (_RealGameTimeCount >= TimeChangerate)
+                {
+                    _RealGameTimeCount = 0;
+                    if (GamedaycountString != null)
+                    {
+                        _GameDayCount += 1; //日计数器累加
+                    Jyx2.Jyx2LuaBridge.jyx2_SetFlag(GamedaycountString, _GameDayCount.ToString());//存储日期数据
+                    }
+                }
+            }
+            float sdeg = ( Time.deltaTime * timetoangle) % 360;//不大于360°
+            MainLight.transform.Rotate(Vector3.right * sdeg);
+           //Rotatex = MainLight.transform.eulerAngles.x;
+        }
     }
 }
